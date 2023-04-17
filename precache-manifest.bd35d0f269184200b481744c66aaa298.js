87a229839b1a151c4e8dcb2c58c84728c7f6610c
Code Sandbox <hello@codesandbox.io>
2023-04-17 02:41:58 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.bd35d0f269184200b481744c66aaa298.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578294959e789fcbb88e4d700ddb54e6",
+    "url": "https://.github.io/csb-1gqxfw/index.html"
+  },
+  {
+    "revision": "8aaf4a4d5f6c7d6dcefc",
+    "url": "https://.github.io/csb-1gqxfw/static/css/main.a056ab23.chunk.css"
+  },
+  {
+    "revision": "4945e99a80d57eda3768",
+    "url": "https://.github.io/csb-1gqxfw/static/js/2.db32b013.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://.github.io/csb-1gqxfw/static/js/2.db32b013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aaf4a4d5f6c7d6dcefc",
+    "url": "https://.github.io/csb-1gqxfw/static/js/main.3fb61573.chunk.js"
+  },
+  {
+    "revision": "63ad4a354920420e0434",
+    "url": "https://.github.io/csb-1gqxfw/static/js/runtime-main.4711c678.js"
+  },
+  {
+    "revision": "a1913f883278367eb7912b9f514181b4",
+    "url": "https://.github.io/csb-1gqxfw/static/media/50-tobylane.a1913f88.mp3"
+  },
+  {
+    "revision": "6a28325ff041f8213d4f77ae0ad2be47",
+    "url": "https://.github.io/csb-1gqxfw/static/media/cali-wataboi.6a28325f.mp3"
+  },
+  {
+    "revision": "01f6eb99187bc00c44214e6116673a99",
+    "url": "https://.github.io/csb-1gqxfw/static/media/iwonder-dreamheaven.01f6eb99.mp3"
+  },
+  {
+    "revision": "60585ee7bcd43b827e5be952828ebd79",
+    "url": "https://.github.io/csb-1gqxfw/static/media/next.60585ee7.svg"
+  },
+  {
+    "revision": "28d702d39257e9eee89945faf4ae275d",
+    "url": "https://.github.io/csb-1gqxfw/static/media/pause.28d702d3.svg"
+  },
+  {
+    "revision": "359e87c77bdaffacb3b0c03e763f0bfb",
+    "url": "https://.github.io/csb-1gqxfw/static/media/play.359e87c7.svg"
+  },
+  {
+    "revision": "a5790adb0648ac5a1b0ae88e6270dd13",
+    "url": "https://.github.io/csb-1gqxfw/static/media/prev.a5790adb.svg"
+  }
+]);
